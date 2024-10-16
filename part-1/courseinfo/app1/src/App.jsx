2ec70772af457b1db6c4f913285e84380dd0e9fe
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-10-16 12:18:26 +0300

    part 1.1-->1.5

--- part-1/courseinfo/app1/src/App.jsx
+++ part-1/courseinfo/app1/src/App.jsx
@@ -1,25 +1,26 @@
 const App = () => {
-    const course = 'Half Stack application development';
-
-    const parts = [
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
         {
           name: 'Fundamentals of React',
           exercises: 10
-          },
+        },
         {
           name: 'Using props to pass data',
           exercises: 7
-          },
+        },
         {
           name: 'State of a component',
           exercises: 14
         }
-    ];
+      ]  
+    }
 
     const Header = ({course}) => {
       console.log('Header: ', course);
       return (
-        <h1>{course}</h1>
+        <h1>{course.name}</h1>
       )
     }
 
@@ -53,8 +54,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
